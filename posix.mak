76b631a939ad8429f93d8712cedbd0e721658fb4
Robert burner Schadek <rburners@gmail.com>
2013-08-19 16:43:37 +0200

    std.logger
    some more docu fixes
    
    fatel -> fatal
    
    changed the way of calling
    
    some rework
    
    some better comments and win32.mak fix
    
    more documentation
    
    trying to figure out why win32 fails
    
    test before you commit
    
    another try to work around windows files and processes
    
    maybe this time
    
    maybe this merges
    
    update 1 mostly soenke
    
    MultiLogger and Logger have names
    
    more docu
    
    unittest fix
    
    some better comments and win32.mak fix
    
    Conflicts:
    	std/logger.d
    
    antoher doc fix
    
    less code dup and more log functions
    
    docs are now generated
    
    some more fixing
    
    speedup
    
    some threading
    
    forgot some
    
    better docu gen and more testing
    
    two new LogLevel, most functions are now at least @trusted
    
    Tracer functionality
    
    fatal delegate
    
    some phobos style fixes
    
    another bug bites the dust
    
    version disable enhancements
    
    default global LogLevel set to LogLevel.all
    
    logLevel compare bugfix
    
    delete of dead code
    
    tab to whitespace
    
    bugfixes, reduandency removal, and docu
    
    multilogger was logging to all it childs without checking there LogLevel in
    relation to the LogLevel of the LoggerPayload.
    
    Some constructors where redundant.
    
    more examples and more documentation
    
    some fixes
    
    NullLogger and moduleName
    
    wrong doc
    
    I splitted the multi logger implementations out
    
    loglevelF to loglevelf in order to make phobos style think writefln
    
    document building
    
    win makefile fixes
    
    some optimizations thanks to @elendel-
    some whitespace
    
    some updates from the github logger project
    
    * stdio- and filelogger now use a templatelogger base to reduce code
    
    makefile fixes
    
    makefile
    
    fixed the unittest
    
    made sure the stdiologger is set up at the end
    
    some comment fixes
    
    finding the filelogger segfault
    
    closed another file
    
    a lookup fix
    
    darwin unittest fail output
    
    more diagnostics
    
    * more documentation for the templatelogger and multilogger
    * it breaks the log function api
     * log and logf now behave as their write and writef counterparts
     * for logging with an explicit LogLevel call logl loglf
     * for logging with an explicit condition call logc logcf
     * for logging with an explicit LogLevel and explicit condition call
     * loglc loglcf
     * the log function with an explicit LogLevel like info, warning, ...
     * can be extended into c, f or cf and therefore require a condition
     * and/or are printf functions
    
    something is still interesting
    
    rebase and lazy Args
    
    saver file handling
    
    whitespace
    
    some updates
    
    tracer is gone and more doc updates

--- posix.mak
+++ posix.mak
@@ -87,7 +87,7 @@ DOCSRC = ../dlang.org
 WEBSITE_DIR = ../web
 DOC_OUTPUT_DIR = $(WEBSITE_DIR)/phobos-prerelease
 BIGDOC_OUTPUT_DIR = /tmp
-SRC_DOCUMENTABLES = index.d $(addsuffix .d,$(STD_MODULES) $(STD_NET_MODULES) $(STD_DIGEST_MODULES) $(STD_CONTAINER_MODULES) $(EXTRA_DOCUMENTABLES))
+SRC_DOCUMENTABLES = index.d $(addsuffix .d,$(STD_MODULES) $(STD_NET_MODULES) $(STD_DIGEST_MODULES) $(STD_CONTAINER_MODULES) $(EXTRA_DOCUMENTABLES)) $(STD_LOGGER_MODULES))
 STDDOC = $(DOCSRC)/std.ddoc
 BIGSTDDOC = $(DOCSRC)/std_consolidated.ddoc
 # Set DDOC, the documentation generator
@@ -185,6 +185,7 @@ STD_MODULES = $(addprefix std/, algorithm array ascii base64 bigint \
 		typecons typetuple uni uri utf uuid variant xml zip zlib)
 
 STD_NET_MODULES = $(addprefix std/net/, isemail curl)
+STD_LOGGER_MODULES = $(addprefix std/logger/, core filelogger stdiologger templatelogger nulllogger multilogger)
 
 STD_REGEX_MODULES = $(addprefix std/regex/, package $(addprefix internal/, \
 	generator ir parser backtracking kickstart tests thompson))
@@ -219,7 +220,9 @@ EXTRA_MODULES += $(EXTRA_DOCUMENTABLES) $(addprefix			\
 
 # Aggregate all D modules relevant to this build
 D_MODULES = $(STD_MODULES) $(EXTRA_MODULES) $(STD_NET_MODULES) \
-	$(STD_DIGEST_MODULES) $(STD_CONTAINER_MODULES) $(STD_REGEX_MODULES)
+	$(STD_DIGEST_MODULES) $(STD_CONTAINER_MODULES) $(STD_REGEX_MODULES) \
+	$(STD_LOGGER_MODULES)
+
 # Add the .d suffix to the module names
 D_FILES = $(addsuffix .d,$(D_MODULES))
 # Aggregate all D modules over all OSs (this is for the zip file)
@@ -414,6 +417,9 @@ $(DOC_OUTPUT_DIR)/std_container_%.html : std/container/%.d $(STDDOC)
 	$(DDOC) project.ddoc $(STDDOC) -Df$@ $<
 
 $(DOC_OUTPUT_DIR)/std_net_%.html : std/net/%.d $(STDDOC)
+	$(DDOC) project.ddoc $(STDDOC) -Df$@ $<
+
+$(DOC_OUTPUT_DIR)/std_logger_%.html : std/logger/%.d $(STDDOC)
 	$(DDOC) project.ddoc $(STDDOC) -Df$@ $<
 
 $(DOC_OUTPUT_DIR)/std_digest_%.html : std/digest/%.d $(STDDOC)
---
+++ std/logger/multilogger.d
@@ -0,0 +1,177 @@
+module std.logger.multilogger;
+
+import std.logger.core;
+import std.logger.stdiologger;
+
+/** MultiLogger logs to multiple logger.
+
+It can be used to construct arbitrary, tree like structures. Basically a $(D
+MultiLogger) is a map. It maps $(D Logger)s to $(D strings)s. By adding a $(D
+MultiLogger) into another $(D MultiLogger) a non leaf nodes is added into the
+tree. The map is implemented as an associated array by the mapper
+$(D MultiLogger.logger).
+
+Example:
+--------------
+/+
+
+root -> node -> b
+|       |
+|       |-> c
+|-> a
+
++/
+
+auto root = new MultiLogger("root", LogLevel.trace);
+auto node = new MultiLogger("Node", LogLevel.warning);
+
+auto a = new StdioLogger("a", LogLevel.trace);
+auto b = new StdioLogger("b", LogLevel.info);
+auto c = new StdioLogger("c", LogLevel.info);
+
+root.insert(node);
+root.insert(a);
+
+node.insert(b);
+node.insert(c);
+--------------
+
+*/
+class MultiLogger : Logger
+{
+    /** Default constructor for the $(D MultiLogger) Logger.
+
+    Params:
+      lv = The $(D LogLevel) for the $(D MultiLogger). By default the $(D LogLevel)
+      for $(D MultiLogger) is $(D LogLevel.info).
+
+    Example:
+    -------------
+    auto l1 = new MultiLogger;
+    auto l2 = new MultiLogger(LogLevel.fatal);
+    -------------
+    */
+    public this(const LogLevel lv = LogLevel.info) @safe
+    {
+        this("", lv);
+    }
+
+    /** A constructor for the $(D MultiLogger) Logger.
+
+    Params:
+      name = The name of the logger. Compare to $(D FileLogger.insertLogger).
+      lv = The $(D LogLevel) for the $(D MultiLogger). By default the
+      $(D LogLevel) for $(D MultiLogger) is $(D LogLevel.info).
+
+    Example:
+    -------------
+    auto l1 = new MultiLogger("loggerName");
+    auto l2 = new MultiLogger("loggerName", LogLevel.fatal);
+    -------------
+    */
+    public this(string name, const LogLevel lv = LogLevel.info) @safe
+    {
+        super(name, lv);
+    }
+
+    /** This member holds all important data of the $(D MultiLogger).
+
+    As mentioned earlier a $(D MultiLogger) is map, this associative array is
+    the mapping.
+    */
+    public Logger[string] logger;
+
+    /** This method inserts a new Logger into the Multilogger.
+    */
+    public void insertLogger(Logger newLogger) @safe
+    {
+        import std.array;
+        if (newLogger.name.empty)
+        {
+            throw new Exception("A Logger must have a name to be inserted " ~
+                "into the MulitLogger");
+        }
+        else if (newLogger.name in logger)
+        {
+            throw new Exception(
+                "This MultiLogger instance already holds a  Logger named '" ~
+                   newLogger.name ~ "'");
+        }
+        else
+        {
+            logger[newLogger.name] = newLogger;
+        }
+    }
+
+    ///
+    unittest
+    {
+        auto l1 = new MultiLogger;
+        auto l2 = new StdIOLogger("some_logger");
+
+        l1.insertLogger(l2);
+
+        assert(l1.removeLogger("some_logger") is l2);
+    }
+
+    /** This method removes a Logger from the Multilogger.
+
+    See_Also: std.logger.MultiLogger.insertLogger
+    */
+    public Logger removeLogger(string loggerName) @safe
+    {
+        if (loggerName !in logger)
+        {
+            throw new Exception(
+                "This MultiLogger instance does not hold a Logger named '" ~
+                loggerName ~ "'");
+        }
+        else
+        {
+            Logger ret = logger[loggerName];
+            logger.remove(loggerName);
+            return ret;
+        }
+    }
+
+    /** This method returns a $(D Logger) if it is present in the $(D
+    MultiLogger), otherwise a $(D RangeError) will be thrown.
+    */
+    public Logger opIndex(string key) @safe
+    {
+        return logger[key];
+    }
+
+    ///
+    unittest
+    {
+        auto ml = new MultiLogger();
+        auto sl = new StdIOLogger("some_name");
+
+        ml.insertLogger(sl);
+
+        assert(ml["some_name"] is sl);
+    }
+
+    public override void writeLogMsg(ref LoggerPayload payload) @trusted {
+        version(DisableMultiLogging)
+        {
+        }
+        else
+        {
+            foreach (it; logger)
+            {
+                /* The LogLevel of the Logger must be >= than the LogLevel of
+                the payload. Usally this is handled by the log functions. As
+                they are not called in this case, we have to handle it by hand
+                here.
+                */
+                const bool ll = payload.logLevel >= it.logLevel;
+                if (ll)
+                {
+                    it.writeLogMsg(payload);
+                }
+            }
+        }
+    }
+}
